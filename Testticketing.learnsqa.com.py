bb068e874c40db7e21b00950be1c94215623ecb7
Kazi Kaiser <kkaiser.eco@gmail.com>
2024-03-11 19:50:47 +0600

    Test online ticketing page scripts upload

---
+++ Testticketing.learnsqa.com.py
@@ -0,0 +1,29 @@
+# Selenium Python Task-1:
+# 1. Go to URL: https://ticketing.learnsqa.com/
+# 2. Input Name, Email, Title, Content
+# 3. Upload a attachment
+# 4. Verify ticket submission is successful
+
+# Selenium Python Task-2:
+# 1. Go to URL: https://ticketing.learnsqa.com/
+# 2. Go to login page
+# 3. Input Email and Password
+# 4. Check the rememberm me checkbox
+# 5. Click on login
+# 6. Verify login is successful
+# 7. Click on user management manu
+# 8. Go to permission page
+# 9. Click on Show entries and select 50
+# 10. Search any ID from searchbox
+# 11. View the ID details by click on View button
+# 12. Click on back to list
+# 13. Check Edit and Delete button by clicking of any of the ID's
+# 14. Verify Action buttons work succesfuly.
+# 15. Go to Ticlets page
+# 16. View any of the ID
+# 17. Scrolling down
+# 18. Leave a comment on commentbox
+# 19. Verify comment submission successful
+# 20. Click on Back to list button
+# 21. Click on Logout
+# 22. Verify successfully logout.
